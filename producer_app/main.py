593ccb4951f1ff8d9614db89c48444ac091ebeac
Nour Samir <noursamir96@gmail.com>
2022-05-23 13:28:11 +0200

    run producer app infinitely

--- producer_app/main.py
+++ producer_app/main.py
@@ -5,8 +5,9 @@ from producer_app.configs import *
 def main():
     producer = Producer()
     raw_data_generator = DataGenerator()
-    for message in raw_data_generator.randomly_generate_data():
-        producer.push_to_topic(topic_name=KAFKA_TOPIC_NAME, message=message)
+    while True:
+        for message in raw_data_generator.randomly_generate_data():
+            producer.push_to_topic(topic_name=KAFKA_TOPIC_NAME, message=message)
 
 
 if __name__ == '__main__':
